18f7506dc666236815a4a504a8822b81da890215
JOI LIU (ITA-ISDC-ISD-OOCLL/ZHA) <LIUJO5@OOCL.COM>
2019-07-10 12:34:31 +0800

    refactor code and add two test

--- __tests__/posMachine-spec.js
+++ __tests__/posMachine-spec.js
@@ -1,72 +1,91 @@
-const posMachine=require('../src/posMachine');
+const posMachine = require('../src/posMachine');
 
-const allItems=[
-    {"id": "0001", "name" : "Coca Cola", "price": 3},
-    {"id": "0002", "name" : "Diet Coke", "price": 4},
-    {"id": "0003", "name" : "Pepsi-Cola", "price": 5},
-    {"id": "0004", "name" : "Mountain Dew", "price": 6},
-    {"id": "0005", "name" : "Dr Pepper", "price": 7},
-    {"id": "0006", "name" : "Sprite", "price": 8},
-    {"id": "0007", "name" : "Diet Pepsi", "price": 9},
-    {"id": "0008", "name" : "Diet Mountain Dew", "price": 10},
-    {"id": "0009", "name" : "Diet Dr Pepper", "price": 11},
-    {"id": "0010", "name" : "Fanta", "price": 12}
+const allItems = [
+  {"id": "0001", "name": "Coca Cola", "price": 3},
+  {"id": "0002", "name": "Diet Coke", "price": 4},
+  {"id": "0003", "name": "Pepsi-Cola", "price": 5},
+  {"id": "0004", "name": "Mountain Dew", "price": 6},
+  {"id": "0005", "name": "Dr Pepper", "price": 7},
+  {"id": "0006", "name": "Sprite", "price": 8},
+  {"id": "0007", "name": "Diet Pepsi", "price": 9},
+  {"id": "0008", "name": "Diet Mountain Dew", "price": 10},
+  {"id": "0009", "name": "Diet Dr Pepper", "price": 11},
+  {"id": "0010", "name": "Fanta", "price": 12}
 ];
 
-it('should return the sum of items\'price when invoke computePrice given specify items',()=>{
-    //give
-    var items = [
-        {"count": 2, "id": "0001", "name": "Coca Cola", "price": 3},
-        {"count": 1, "id": "0002", "name": "Diet Coke", "price": 4},
-        {"count": 1, "id": "0003", "name": "Pepsi-Cola", "price": 5}];
-    //when
-    const price = posMachine.computePrice(items);
-    //then
-    expect(price).toBe(15);
-});
-it('should return items with count when invoke findItems given item 0001 0001 0002 0003',()=>{
-    //give
-    const items=['0001', '0001', '0002', '0003'];
-    //when
-    const orderedItemsWithCount = posMachine.findItems(items,allItems);
-    //then
-    expect(orderedItemsWithCount).toEqual([
-        {"count":2,"id": "0001", "name" : "Coca Cola", "price": 3},
-        {"count":1,"id": "0002", "name" : "Diet Coke", "price": 4},
-        {"count":1,"id": "0003", "name" : "Pepsi-Cola", "price": 5}]);
+it('should return the sum of items\'price when invoke computePrice given specify items', () => {
+  //give
+  const items = [
+    {"count": 2, "id": "0001", "name": "Coca Cola", "price": 3},
+    {"count": 1, "id": "0002", "name": "Diet Coke", "price": 4},
+    {"count": 1, "id": "0003", "name": "Pepsi-Cola", "price": 5}];
+  //when
+  const price = posMachine.calculateTotalPrice(items);
+  //then
+  expect(price).toBe(15);
 });
 
-it('should return receipts when invoke createReceipt given item 0001 0001 0002 0003',()=>{
-    //give
-    const items=[
-        {"count":2,"id": "0001", "name" : "Coca Cola", "price": 3},
-        {"count":1,"id": "0002", "name" : "Diet Coke", "price": 4},
-        {"count":1,"id": "0003", "name" : "Pepsi-Cola", "price": 5}];
-    //when
-    const receipts = posMachine.createReceipt(15,items);
-    //then
-    expect(receipts).toEqual(
-        'Receipts\n' +
-        '------------------------------------------------------------\n' +
-        '\tCoca Cola\t\t\t3\t2\n' +
-        '\tDiet Coke\t\t\t4\t1\n' +
-        '\tPepsi-Cola\t\t\t5\t1\n' +
-        '------------------------------------------------------------\n' +
-        'Price: 15');
+it('should return false when invoke isValid given barcodes 01', () => {
+  //give
+  const barcodes = ['01'];
+  //when
+  const flag = posMachine.isValid(barcodes,allItems);
+  //then
+  expect(flag).toBe(false);
 });
 
-it('should return receipts when invoke printReceipt given item 0001 0003 0005 0003',()=>{
-    //give
-    const barcodes=['0001', '0003', '0005', '0003'];
-    //when
-    const receipts = posMachine.printReceipt(barcodes,allItems);
-    //then
-    expect(receipts).toEqual(
-        'Receipts\n' +
-        '------------------------------------------------------------\n' +
-        '\tCoca Cola\t\t\t3\t1\n' +
-        '\tPepsi-Cola\t\t\t5\t2\n' +
-        '\tDr Pepper\t\t\t7\t1\n' +
-        '------------------------------------------------------------\n' +
-        'Price: 20');
+it('should return true when invoke isValid given barcodes 0001 0003 0005 0003', () => {
+  //give
+  const barcodes = ['0001', '0003', '0005', '0003'];
+  //when
+  const flag = posMachine.isValid(barcodes,allItems);
+  //then
+  expect(flag).toBe(true);
+});
+
+it('should return items with count when invoke findItems given item 0001 0001 0002 0003', () => {
+  //give
+  const items = ['0001', '0001', '0002', '0003'];
+  //when
+  const orderedItemsWithCount = posMachine.findItems(items, allItems);
+  //then
+  expect(orderedItemsWithCount).toStrictEqual([
+    {"count": 2, "id": "0001", "name": "Coca Cola", "price": 3},
+    {"count": 1, "id": "0002", "name": "Diet Coke", "price": 4},
+    {"count": 1, "id": "0003", "name": "Pepsi-Cola", "price": 5}]);
+});
+
+it('should return receipts when invoke createReceipt given item 0001 0001 0002 0003', () => {
+  //give
+  const items = [
+    {"count": 2, "id": "0001", "name": "Coca Cola", "price": 3},
+    {"count": 1, "id": "0002", "name": "Diet Coke", "price": 4},
+    {"count": 1, "id": "0003", "name": "Pepsi-Cola", "price": 5}];
+  //when
+  const receipts = posMachine.createReceipt(15, items);
+  //then
+  expect(receipts).toEqual(
+      'Receipts\n' +
+      '------------------------------------------------------------\n' +
+      '\tCoca Cola\t\t\t3\t2\n' +
+      '\tDiet Coke\t\t\t4\t1\n' +
+      '\tPepsi-Cola\t\t\t5\t1\n' +
+      '------------------------------------------------------------\n' +
+      'Price: 15');
+});
+
+it('should return receipts when invoke printReceipt given item 0001 0003 0005 0003', () => {
+  //give
+  const barcodes = ['0001', '0003', '0005', '0003'];
+  //when
+  const receipts = posMachine.printReceipt(barcodes, allItems);
+  //then
+  expect(receipts).toEqual(
+      'Receipts\n' +
+      '------------------------------------------------------------\n' +
+      '\tCoca Cola\t\t\t3\t1\n' +
+      '\tPepsi-Cola\t\t\t5\t2\n' +
+      '\tDr Pepper\t\t\t7\t1\n' +
+      '------------------------------------------------------------\n' +
+      'Price: 20');
 });
--- src/posMachine.js
+++ src/posMachine.js
@@ -1,52 +1,64 @@
-function printReceipt(barcodes,allItems) {
-    try {
-        var orderedItemsWithCount = findItems(barcodes, allItems);
-        var prices = computePrice(orderedItemsWithCount);
-        var receipts = createReceipt(prices, orderedItemsWithCount);
-    } catch (e) {
-        var err='[ERROR]:';
-        return err+e;
+function printReceipt(barcodes, allItems) {
+    if (isValid(barcodes, allItems)) {
+      let orderedItemsWithCount = findItems(barcodes, allItems);
+      let prices = calculateTotalPrice(orderedItemsWithCount);
+      var receipts = createReceipt(prices, orderedItemsWithCount);
+    }else{
+      receipts='[ERROR]:The barcodes are not valid.';
     }
-    return receipts;
+  return receipts;
+}
+
+function isValid(barcodes,allItems) {
+  let itemIds=[];
+  for(let item of allItems){
+    itemIds.push(item.id);
+  }
+  for(let barcode of barcodes){
+    if (itemIds.indexOf(barcode) == -1) {
+      return false;
+    }
+  }
+  return true;
 }
 
 function findItems(barcodes, allItems) {
-    var orderedItems = [];
-    for (var item of allItems) {
-        item['count'] = 0;
-        for (var barcode of barcodes) {
-            if (item.id == barcode) {
-                if (orderedItems.indexOf(item)>-1) {
-                    orderedItems.splice(orderedItems.indexOf(item), 1);
-                }
-                item['count']++;
-                orderedItems.push(item);
-            }
+  let orderedItems = [];
+  for (let item of allItems) {
+    item['count'] = 0;
+    for (let barcode of barcodes) {
+      if (item.id == barcode) {
+        if (orderedItems.indexOf(item) > -1) {
+          orderedItems.splice(orderedItems.indexOf(item), 1);
         }
+        item['count']++;
+        orderedItems.push(item);
+      }
     }
-    return orderedItems;
+  }
+  return orderedItems;
 }
 
-function computePrice(orderedItems) {
-    var sum = 0;
-    for (item of orderedItems) {
-        sum += item.price*item.count;
-    }
-    return sum;
+function calculateTotalPrice(orderedItems) {
+  let sum = 0;
+  for (item of orderedItems) {
+    sum += item.price * item.count;
+  }
+  return sum;
 }
 
-function createReceipt(price,orderedItemsWithCount){
-    var receipts;
-    receipts='Receipts\n' +
-        '------------------------------------------------------------\n';
-    for (var item of orderedItemsWithCount) {
-        receipts += '\t'+item.name + '\t\t\t' + item.price + '\t' + item.count + '\n';
-    }
-    receipts+='------------------------------------------------------------\n'+
-        'Price: '+price;
-    return receipts;
+function createReceipt(price, orderedItemsWithCount) {
+  let receipts;
+  receipts = 'Receipts\n' +
+      '------------------------------------------------------------\n';
+  for (let item of orderedItemsWithCount) {
+    receipts += '\t' + item.name + '\t\t\t' + item.price + '\t' + item.count + '\n';
+  }
+  receipts += '------------------------------------------------------------\n' +
+      'Price: ' + price;
+  return receipts;
 }
 
 module.exports = {
-    printReceipt, computePrice, findItems,createReceipt,
+  printReceipt, calculateTotalPrice, findItems, createReceipt,isValid
 };
